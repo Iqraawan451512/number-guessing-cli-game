0dc708af223e6adb9a5130435c5999b6cfb3ed31
Iqra <148074762+Iqraawan451512@users.noreply.github.com>
2024-03-27 06:37:07 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 +1);
+const answer = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"please gussed a number between 1-6:",
+    },
+]);
+
+
+if (answer.userGuessedNumber === randomNumber){
+    console.log("congratulation! you gassed a right number");
+}
+else
+{
+        console.log(" sorry  you gussed a wronge number, please try again");
+}
+
